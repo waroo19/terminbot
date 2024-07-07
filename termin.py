982e0433ac583e07a80451e3836556f0d619a8b1
Nihal Alizada <96690541+nihalalizada@users.noreply.github.com>
2024-07-07 11:20:10 +0200

    auto check termin

--- termin.py
+++ termin.py
@@ -8,7 +8,8 @@ telegram_chat_id = os.getenv("TELEGRAM_CHAT_ID")
 
 # URLs
 session_cookie_source_url = "https://tevis.ekom21.de/fra/select2?md=35"
-appointment_check_url = "https://tevis.ekom21.de/fra/location?mdt=217&select_cnc=1&cnc-1039=1"
+appointment_check_url = f"https://tevis.ekom21.de/fra/suggest?loc=30&mdt=0&cnc-1039=1&filter_date_from={today_date}&filter_date_to=30.08.2024"
+print(appointment_check_url)
 telegram_send_chat_url = f"https://api.telegram.org/bot{telegram_bot_secret}/sendMessage?chat_id={telegram_chat_id}&text="
 
 def send_telegram_chat(msg):
@@ -17,24 +18,61 @@ def send_telegram_chat(msg):
         print("Something went wrong with sending telegram messages, are the keys correct?")
         print(response.text)
 
-# if error state file is present, something went wrong so quit the script
+driver = webdriver.Chrome()
+driver.get(session_cookie_source_url)
+
+
+try:
+    wait = WebDriverWait(driver, 5)
+    cookie_dismiss_button = wait.until(EC.element_to_be_clickable((By.ID, 'dismiss_button_id')))
+    cookie_dismiss_button.click()
+except Exception as e:
+    print(f"No cookie dialog found or already handled: {e}")
+
+try:
+    plus_button = wait.until(EC.element_to_be_clickable((By.ID, 'button-plus-1039')))
+    plus_button.click()
+except Exception as e:
+    print(f"Failed to click the '+' button: {e}")
+
+try:
+    weiter_button = wait.until(EC.element_to_be_clickable((By.ID, 'WeiterButton')))
+    weiter_button.click()
+except Exception as e:
+    print(f"Failed to click the 'Weiter' button: {e}")
+
+try:
+    WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.ID, 'TevisDialog')))
+    ok_button = driver.find_element(By.ID, 'OKButton')
+    ok_button.click()
+    print("'OK' button clicked")
+    print(appointment_check_url)
+except Exception as e:
+    print(f"Modal handling error: {e}")
+
+try:
+    button = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.NAME, "select_location"))
+    )
+    driver.execute_script("arguments[0].click();", button)
+    print("Button clicked using JavaScript")
+except Exception as e:
+    print("Failed to click the button using JavaScript:", e)
+
+selenium_cookies = driver.get_cookies()
+cookie_dict = {cookie['name']: cookie['value'] for cookie in selenium_cookies}
+driver.quit()
+
 if os.path.exists('./error.txt'):
     print("Error state :(")
     quit()
 
-# get session cookie first
-session_request = requests.get(session_cookie_source_url)
-
-# send appointment check request with session cookie
-check_request = requests.get(appointment_check_url, cookies=session_request.cookies)
-
-if "aktuelle Anliegenauswahl ist leider kein Termin" in check_request.text:
-    # send_telegram_chat("Kein Termin verfügbar")
-    pass
+check_request = requests.get(appointment_check_url, cookies=cookie_dict)
+if "Keine Zeiten verfügbar" in check_request.text:
+    send_telegram_chat("Kein Termin verfügbar")
 elif "Es ist ein Fehler aufgetreten" in check_request.text:
-    # Error state
     send_telegram_chat("Bot machine broke")
-    with open("./error.txt", "w") as f:
+    with open("./error.txt", "w", encoding='utf-8') as f:
         f.write(check_request.text)
 else:
-    send_telegram_chat("Termin verfügbar")
+    send_telegram_chat("Termin verfügbar!")
