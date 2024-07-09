5378f6ac20a1f77611133c91d73bdd2e9b20365c
Nihal Alizada <96690541+nihalalizada@users.noreply.github.com>
2024-07-09 15:11:01 +0200

    Update chrome_driver.py

--- chrome_driver.py
+++ chrome_driver.py
@@ -4,11 +4,15 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 def get_chromedriver(headless=True):
-    options = Options()
+    chrome_options = Options()
     if headless:
-        options.headless = True  # Run in headless mode if needed
-
-    # Use webdriver-manager to manage ChromeDriver
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=options)
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--window-size=1920x1080")
+        chrome_options.add_argument("--remote-debugging-port=9222")
+        
+    chrome_service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
     return driver
