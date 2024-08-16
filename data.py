a331b76ac899b5d35ea42900ae1575abf0e96593
Nihal Alizada <96690541+nihalalizada@users.noreply.github.com>
2024-08-16 11:38:24 +0200

    Update data.py

--- data.py
+++ data.py
@@ -1,14 +1,14 @@
 personal_info = {
     'sex': 'm', 
-    'vorname': 'Nihal Ahmed',
-    'nachname': 'Alizada',
-    'email': 'nihalhaleem@gmail.com',
-    'emailCheck': 'nihalhaleem@gmail.com',
-    'geburtsdatumDay': '12',
-    'geburtsdatumMonth': '11',
-    'geburtsdatumYear': '1997',
-    'anschrift': 'Siolistrasse 7, 6101',
-    'postleitzahl': '60323',
+    'vorname': 'vorname',
+    'nachname': 'nachname',
+    'email': 'email',
+    'emailCheck': 'email',
+    'geburtsdatumDay': '1',
+    'geburtsdatumMonth': '1',
+    'geburtsdatumYear': '2000',
+    'anschrift': 'Musterstr',
+    'postleitzahl': '60318',
     'wohnort': 'Frankfurt am Main',
     'captcha_code': '12345'  # How to handle this? TODO!
 }
